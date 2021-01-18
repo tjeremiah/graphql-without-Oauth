a5b6d9ae7d3740df22a8fe966a82e1e3963b6f8e
Torrel <taurel107@gmail.com>
2021-01-17 20:28:37 -0500

    accessed albums and  artist from graphQL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client"
+import AlbumContainer from './containers/AlbumContainer'
 
-function App() {
+//Initialize Apollo Client
+const client = new ApolloClient ({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql" // graphql server link
+  })
+
+})
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client = {client} >
+      <AlbumContainer />
+    </ApolloProvider>
+    
   );
 }
 
---
+++ src/containers/AlbumContainer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useQuery , gql } from '@apollo/client'
+import { Album } from '../components/Album'
+
+const ALL_ALBUMS = gql`
+  query album_tracks {
+   album_tracks {
+      items {
+       id
+       name
+      external_urls {
+        spotify
+      }
+      artists {
+        id  
+        name
+        album
+        external_urls {
+            spotify
+        }
+      }
+    }
+   }
+  }
+ `;
+
+export default function AlbumContainer () {
+    const { loading, error, data } = useQuery(ALL_ALBUMS)
+    console.log("the items are")
+    if (loading ) return <p>Loading Albums...</p>
+
+    if (error) return <p>Something went wrong</p>
+    return (
+        <div className="container">
+         {data.album_tracks.items && data.album_tracks.items.map(item => <Album key={item.id} item={item} /> )}
+        </div>
+    )
+}
